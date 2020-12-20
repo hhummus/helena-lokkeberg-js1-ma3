9b08f89aee7a3fa7ff0268ae334216844d874e7a
hhummus <helena_l93@hotmail.com>
2020-12-20 15:43:59 +0100

    initial commit git push origin https://github.com/hhummus/helena-lokkeberg-js1-ma3.git git status

---
+++ question-2.js
@@ -0,0 +1,36 @@
+// Question 2
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFix = proxy + url;
+
+const divHtml = document.querySelector("div");
+
+async function makeApiCall(){
+    try{
+        const response = await fetch(url);
+        const results = await response.json();
+
+        console.log(results)
+        console.log(results.results)
+
+       
+
+        for(let i = 0; i <= 8; i++) {
+            console.log(results.results[i].name)
+            console.log(results.results[i].rating)
+            console.log(results.results[i].tags)
+            const newDiv = document.createElement('div');
+            divHtml.appendChild(newDiv)
+            newDiv.innerHTML = results.results[i].name + 
+            results.results[i].rating + results.results[i].tags;
+        }
+      return results;
+
+
+    } catch(error){
+        console.log(error);
+    }
+
+
+};
